c9129b0dbc142a422cb430e9401122f196c829d5
Sam Robinson <pinkstarsr11@gmail.com>
2021-11-13 22:00:50 -0800

    Tiled reset

--- Tiled/Tileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="tileset" tilewidth="16" tileheight="16" tilecount="1024" columns="9">
- <image source="../Content/tileset.png" width="144" height="144"/>
-</tileset>
