e9c68ab9434f22987c2f5f2a8797a99eadb580ce
Samuel Robinson <pinkstarsr11@gmail.com>
2021-12-03 16:04:29 -0800

    Undo those debugging goodies

--- TwelveEngine/GameState/GameManager.cs
+++ TwelveEngine/GameState/GameManager.cs
@@ -102,7 +102,7 @@ namespace TwelveEngine {
             pendingGameState = gameState;
         }
 
-        private byte[] savedState = null;
+        private SerialFrame savedState = null;
         private void saveSerialState() { /* This could be made async, but the game could change
                                     * during the saving process, creating an unstable save state */
             if(!hasGameState()) {
@@ -110,14 +110,14 @@ namespace TwelveEngine {
             }
             var frame = new SerialFrame();
             gameState.Export(frame);
-            savedState = frame.Export();
+            savedState = frame;
         }
         private void loadSerialState() {
             if(!hasGameState() || savedState == null) {
                 return;
             }
-            var frame = new SerialFrame(savedState);
-            gameState.Import(frame);
+            savedState.StartReadback();
+            gameState.Import(savedState);
         }
 
         private bool hasGameState() => gameState != null;
