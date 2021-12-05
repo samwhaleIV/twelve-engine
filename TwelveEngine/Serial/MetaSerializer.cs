eb6cde289efa7c007983a25f240bb528ef99b34b
Samuel Robinson <pinkstarsr11@gmail.com>
2021-12-05 11:08:25 -0800

    Refactor serial frame serializer

--- TwelveEngine/Serial/MetaSerializer.cs
+++ TwelveEngine/Serial/MetaSerializer.cs
@@ -46,17 +46,14 @@ namespace TwelveEngine {
             int arraySize = readInt(data,i);
             i += sizeof(int); //Array size in bytes (counted in int)
             var bytes = new byte[arraySize];
-
-            Copy(data,i,bytes,0,arraySize);
-            
-            if(RequiresByteFlip) {
-                int end = i + arraySize;
-                for(int i2 = i;i2<end;i2+=sizeof(int)) {
-                    swapInt(data,i2);
-                }
+       
+            if(!RequiresByteFlip) {
+                Copy(data,i,bytes,0,bytes.Length);
+            } else {
+                copyArraySwapped(i,data,bytes);
             }
 
-            i += arraySize; //Blob
+            i += bytes.Length; //Blob
             return bytes;
         }
         private static byte[] decodeValue(byte[] data,ref int i,Type type) {
@@ -101,7 +98,7 @@ namespace TwelveEngine {
         private static void swapInt(
             byte[] source,int index
         ) {
-            var buffer = source[index];
+            byte buffer = source[index];
             source[index] = source[index+3];
             source[index+3] = buffer;
             buffer = source[index+1];
@@ -124,14 +121,58 @@ namespace TwelveEngine {
             writeInt(GetBytes(value),destination,index);
         }
 
-        public byte[] Export() {
+        private static void copyArraySwapped(int i,byte[] data,byte[] bytes) {
+            for(int x = 0;x<bytes.Length;x+=sizeof(int)) { 
+                int index = i + x;
+                data[index] = bytes[x+3];
+                data[index+1] = bytes[x+2];
+                data[index+2] = bytes[x+1];
+                data[index+3] = bytes[x];
+            }
+        }
+
+        private static void encodeArray(byte[] data,byte[] bytes,ref int i) {
+            writeInt(bytes.Length,data,i); //Array size in bytes (counted in int)
+            i += sizeof(int);
+
+            if(!RequiresByteFlip) {
+                bytes.CopyTo(data,i); //Blob
+            } else {
+                copyArraySwapped(i,data,bytes); //Blob
+            }
+            i += bytes.Length;
+        }
+
+        private static void encodeValue(byte[] data,int key,Type type,byte[] bytes, ref int i) {
+            writeInt(key,data,i); //Key (int)
+            i += sizeof(int);
+
+            data[i] = (byte)type; //Type (byte)
+            i += sizeof(byte);
+
+            if(arrayType.Contains(type)) {
+                encodeArray(data,bytes,ref i);
+                return;
+            } else if(type == Type.String) {
+                writeInt(bytes.Length,data,i); //String length in bytes (counted in int)
+                i += sizeof(int);
+            }
+
+            bytes.CopyTo(data,i); //Blob
+            if(RequiresByteFlip && endianSensitive.Contains(type)) {
+                Reverse(data,i,bytes.Length);
+            }
+            i += bytes.Length;
+        }
+
+        private static int getDataSize(Dictionary<int,Value>.ValueCollection values) {
             int dataSize = 0;
             dataSize += sizeof(int); //Subframe count
 
-            foreach(var set in values.Values) {
+            foreach(var set in values) {
                 dataSize += sizeof(int); //Key (int)
                 dataSize += sizeof(byte); //Type (byte)
-                var type = set.Type;
+                Type type = set.Type;
                 if(arrayType.Contains(type)) {
                     dataSize += sizeof(int); //Array size in bytes (counted in int)
                 } else if(type == Type.String) {
@@ -139,48 +180,24 @@ namespace TwelveEngine {
                 }
                 dataSize += set.Bytes.Length; //Blob
             }
-            byte[] data = new byte[dataSize];
+            return dataSize;
+        }
 
-            var index = 0;
-            writeInt(subframeCount,data,index); //Subframe count
-            index += sizeof(int);
+        public byte[] Export() {
 
+            /* 'values' is the dictionary that contains the (Type,byte[]) struct */
+            byte[] data = new byte[getDataSize(values.Values)];
 
-            foreach(KeyValuePair<int,Value> set in values) {
-                int key = set.Key;
-                Value value = set.Value;
-                Type type = value.Type;
-                byte[] bytes = value.Bytes;
+            var i = 0;
+            writeInt(subframeCount,data,i); //Subframe count
+            i += sizeof(int);
 
-                writeInt(key,data,index); //Key (int)
-                index += sizeof(int);
+            foreach(var set in values) {
 
-                data[index] = (byte)type; //Type (byte)
-                index += sizeof(byte);
+                Type type = set.Value.Type;
+                byte[] bytes = set.Value.Bytes;
 
-                if(arrayType.Contains(type)) {
-
-                    writeInt(bytes.Length,data,index); //Array size in bytes (counted in int)
-                    index += sizeof(int);
-
-                    bytes.CopyTo(data,index); //Blob
-                    index += bytes.Length;
-
-                    if(!RequiresByteFlip) continue;
-                    for(var i = index - bytes.Length;i<index;i+=sizeof(int)) {
-                        swapInt(data,i);
-                    }
-                    continue;
-                } else if(type == Type.String) {
-                    writeInt(bytes.Length,data,index); //String length in bytes (counted in int)
-                    index += sizeof(int); 
-                }
-
-                bytes.CopyTo(data,index); //Blob
-                if(RequiresByteFlip && endianSensitive.Contains(type)) {
-                    Reverse(data,index,bytes.Length);
-                }
-                index += bytes.Length;
+                encodeValue(data,set.Key,type,bytes,ref i);
             }
 
             return data;
